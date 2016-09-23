de2d46511e265b71312ff28d0164a76adb2f443e
k-ko <k-ko@mesena-net.com>
2016-09-23 12:29:45 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MesenaApp::Application.config.secret_key_base = '96b539af39287a3ce2536ea9b9fb846c495846ea6bca048394132d8da0a68db47d6748e7c089c4a10108bd0a6b9a4e8bdfa790d4f8b9d4e18f4fcee489f3919e'
